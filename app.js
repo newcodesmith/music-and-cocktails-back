12d830e8a9c235c26f0cea8866b62932c3cdc913
Michael Smith <michael@Michaels-MacBook-Pro-6.local>
2018-05-08 14:48:04 -0600

    add require-https

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const requireHttps = require('require-https');
 
 const app = express();
 
@@ -13,6 +14,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.enable('trust proxy');
+app.use(requireHttps());
 
 app.use('/albums', albums);
 app.use('/drinks', drinks);
