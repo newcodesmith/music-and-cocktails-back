68da2171157b155c6ee966b472d3676799d30057
Michael Smith <michael@Michaels-MacBook-Pro-6.local>
2018-05-09 13:11:58 -0600

    add drink_id to seed

--- albums-seed-data.js
+++ albums-seed-data.js
@@ -4,41 +4,47 @@ module.exports = [
         artist: "The Beach Boys",
         album_title: "Pet Sounds",
         album_info: "One of the greatest albums of all time.",
-        spotify_album_id: "42oBdomfxF0DbKKMEqrnQW"
+        spotify_album_id: "42oBdomfxF0DbKKMEqrnQW",
+        drink_id: 1
     },
     {
         genre: "Indie",
         artist: "The National",
         album_title: "Trouble Will Find Me",
         album_info: "One of the greatest albums of all time.",
-        spotify_album_id: "2JhR4tjuc3MIKa8v2JaKze"
+        spotify_album_id: "2JhR4tjuc3MIKa8v2JaKze",
+        drink_id: 2
     },
     {
         genre: "R&B",
         artist: "Aretha Franklin",
         album_title: "I Never Loved A Man The Way I Love You",
         album_info: "One of the greatest albums of all time.",
-        spotify_album_id: "5WndWfzGwCkHzAbQXVkg2V"
+        spotify_album_id: "5WndWfzGwCkHzAbQXVkg2V",
+        drink_id: 3
     },
     {
         genre: "Jazz",
         artist: "Miles Davis",
         album_title: "Someday My Prince Will Come",
         album_info: "One of the greatest albums of all time.",
-        spotify_album_id: "68A4o4tkirJRFYbO9Ag0YZ"
+        spotify_album_id: "68A4o4tkirJRFYbO9Ag0YZ",
+        drink_id: 4
     },
     {
         genre: "Folk/Country",
         artist: "Gregory Alan Isakov",
         album_title: "The Weatherman",
         album_info: "One of the greatest albums of all time.",
-        spotify_album_id: "3IcfYXw7lTtLfG5XWLlSZi"
+        spotify_album_id: "3IcfYXw7lTtLfG5XWLlSZi",
+        drink_id: 5
     },
     {
         genre: "Rap",
         artist: "Snoop Dogg",
         album_title: "Doggystyle",
         album_info: "One of the greatest albums of all time.",
-        spotify_album_id: "7f9KDGqY7X2VLBM5aA66KM"
+        spotify_album_id: "7f9KDGqY7X2VLBM5aA66KM",
+        drink_id: 6
     },
 ]
