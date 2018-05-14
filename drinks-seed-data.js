c6a4093e10fd65e45daa40336e54a959a7042978
Michael Smith <michael@Michaels-MacBook-Pro-6.local>
2018-05-14 09:30:47 -0600

    add more drinks

--- drinks-seed-data.js
+++ drinks-seed-data.js
@@ -40,5 +40,48 @@ module.exports = [
         ingredients: "1.5 to 2 oz gin; 5 oz juice; Ice",
         direction: "Shake ingredients with ice. Strain into a rocks glass or a cocktail class. Garnish, such as a fruit wedge or sprig of mint",
         drink_pic_url: "https://s-media-cache-ak0.pinimg.com/originals/45/a5/16/45a516802a0fde7406ddd62cc9c36959.jpg"
-    }, 
+    },
+    {
+        drink_title: "Margarita",
+        drink_description: "Cloyingly sweet margarita mixes have given this drink a bad name. A well-made version is a fresh mix of lime juice and tequila, with a hint of sweetener.",
+        ingredients: "2 oz silver tequila; 1 oz Cointreau; 1 oz lime juice; Salt for the rim",
+        direction: "Since this recipe includes fresh juice, it should be shaken. Serve over ice in a glass with a salted rim.",
+        drink_pic_url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-516883622-1508961864.jpg?crop=0.44377777777777777xw:1xh;center,top&resize=980:*"
+    },
+    {
+        drink_title: "Negroni",
+        drink_description: "A favorite of bartenders all over the world, the Negroni is a simple three-ingredient cocktail.",
+        ingredients: "1 oz gin; 1 oz Campari; 1 oz sweet vermouth",
+        direction: "Stir ingredients with ice.",
+        drink_pic_url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-89742752-1508962023.jpg?crop=0.528xw:1.00xh;0.240xw,0&resize=980:*"
+    },
+    {
+        drink_title: "Manhattan",
+        drink_description: "Created sometime in the mid-1800s, the Manhattan is one of the booziest classic drink recipes.",
+        ingredients: "2 oz rye whiskey; 1 oz sweet vermouth; 2 dashes Angostura bitters",
+        direction: "Stir ingredients in a mixing glass with ice. Strain into chilled martini glass or cocktail coupe",
+        drink_pic_url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-89804127-1508971287.jpg?crop=0.864516129032258xw:1xh;center,top&resize=980:*"
+    },
+    {
+        drink_title: "Mimosa",
+        drink_description: "We'd like to salute Frank Meier, the bartender at the Ritz Paris who in 1925 reportedly served the first mimosa. The recipe just might be the simplest cocktail ever created: just combine equal parts of the ingredients in a champagne flute.",
+        ingredients: "2.5 oz champagne; 2.5 oz orange juice",
+        direction: "Pour both the champagne and orange juice into a champagne glass",
+        drink_pic_url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-88833498-1508963207.jpg?crop=0.8723029502421841xw:1xh;center,top&resize=980:*"
+    },
+    {
+        drink_title: "Mint Julep",
+        drink_description: "The official drink of the Kentucky Derby is worth ordering even when you're not at Churchill Downs.",
+        ingredients: "2 oz bourbon; 8-10 mint leaves; .25 oz simple syrup",
+        direction: "Muddle the mint leaves and simple syrup in a mint julep cup. Add bourbon and fill with with crushed ice. Stir until the cup is frosted. Fill with more crushed ice. Serve with a straw and a mint sprig garnish.",
+        drink_pic_url: "https://hips.hearstapps.com/toc.h-cdn.co/assets/16/21/1464125086-cocktailhistorymintjulep.jpg?crop=0.8877816291161178xw:1xh;center,top&resize=980:*"
+    },
+    {
+        drink_title: "Mojito",
+        drink_description: "The origins of the drink can be traced back to Cuba and the 16th-century Cuban cocktail El Draque, named for Sir Francis Drake. Appropriately, almost all of the ingredients in the Mojito are indigenous to Cuba. Rum, lime, mint and sugar (derived from cane juice) are mixed with soda water to create a refreshing classic cocktail. The Mojito is traditionally served over ice in a highball glass and often associated with summer. Like your cocktails with literary references? You’re in luck. The Mojito is said to have been a favorite of Ernest Hemingway.",
+        ingredients: "6 Mint leaves; 3/4 oz Simple syrup; 3/4 Fresh lime juice; 1 1/2 oz White rum; 1 1/2 oz Club soda",
+        direction: "In a shaker, lightly muddle the mint. Add the simple syrup, lime juice and rum, and fill with ice. Shake well and pour (unstrained) into a highball glass. Top with the club soda and garnish with a mint sprig.",
+        drink_pic_url: "https://cdn.liquor.com/wp-content/uploads/2017/03/07152909/mojito-720x720-recipe.jpg"
+    },
+    
 ]
