2a609c2a95eb6cecf37a6e892f50720a3fe25c9d
Michael <milo0721@gmail.com>
2026-01-28 08:14:38 -0700

    update fixes

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-// const requireHttps = require('require-https');
-var cors = require('cors');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const app = express();
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
 
 const albums = require('./routes/albums-routes');
 const drinks = require('./routes/drinks-routes');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.enable('trust proxy');
-// app.use(requireHttps());
-app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/albums', albums);
 app.use('/drinks', drinks);
@@ -29,11 +32,13 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
   res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: req.app.get('env') === 'development' ? err : {}
-  });
+  res.render('error');
 });
 
 module.exports = app;
--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,15 @@
 // Update with your config settings.
 
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
 module.exports = {
   development: {
-    client: 'pg',
-    connection: 'postgres://localhost/music-and-cocktails'
+    client: "pg",
+    connection: "postgres://localhost/music-and-cocktails",
   },
   production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL
-  }
+    client: "pg",
+    connection: process.env.DATABASE_URL + '?ssl=true',
+  },
 };
