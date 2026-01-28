8e2c749ab68477cf6c48fb7ecfb0626ddbb4a4be
Michael <milo0721@gmail.com>
2026-01-28 10:09:37 -0700

    Some clean up

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 const albums = require('./routes/albums-routes');
