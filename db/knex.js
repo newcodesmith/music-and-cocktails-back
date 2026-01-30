dc97d56c01346dc228828c3db821f72049541cbc
Michael <milo0721@gmail.com>
2026-01-30 08:42:29 -0700

    update

--- db/knex.js
+++ db/knex.js
@@ -1,4 +1,4 @@
-const environment = process.env.NODE_ENV || "development";
+const environment = process.env.NODE_ENV;
 const config = require("../knexfile");
 const environmentConfig = config[environment];
 const knex = require("knex");
