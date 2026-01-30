4e59cc31a98583012a9b66aec4b28160dd5e6278
Michael <milo0721@gmail.com>
2026-01-30 09:06:51 -0700

    testing

--- db/knex.js
+++ db/knex.js
@@ -1,4 +1,4 @@
-const environment = process.env.NODE_ENV || "development";
+const environment = "production";
 const config = require("../knexfile");
 const environmentConfig = config[environment];
 const knex = require("knex");
