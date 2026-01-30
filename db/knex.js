dffe1ffc43f8ec8df8f31a674b8a2fc344ca1dbe
Michael <milo0721@gmail.com>
2026-01-30 08:46:09 -0700

    another update

--- db/knex.js
+++ db/knex.js
@@ -1,4 +1,4 @@
-const environment = process.env.NODE_ENV;
+const environment = process.env.NODE_ENV || "development";
 const config = require("../knexfile");
 const environmentConfig = config[environment];
 const knex = require("knex");
--- knexfile.js
+++ knexfile.js
@@ -11,7 +11,10 @@ module.exports = {
   production: {
     client: "pg",
     connection: {
-      connectionString: process.env.DATABASE_URL,
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       ssl: { rejectUnauthorized: false },
     },
   },
