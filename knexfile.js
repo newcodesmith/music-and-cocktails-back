d77add41cecfc0d7fe5b098719b2e6c7cb089cbb
Michael <milo0721@gmail.com>
2026-01-30 08:33:50 -0700

    Fix postgres connection string

--- knexfile.js
+++ knexfile.js
@@ -10,7 +10,9 @@ module.exports = {
   },
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    },
   },
 };
