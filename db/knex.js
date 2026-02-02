09a201179591168479ec778edcccc02ae74c50d1
Michael <milo0721@gmail.com>
2026-02-02 08:21:48 -0700

    update

--- db/knex.js
+++ db/knex.js
@@ -1,7 +1,11 @@
-const environment = process.env.NODE_ENV || "development";
-const config = require("../knexfile");
-const environmentConfig = config[environment];
-const knex = require("knex");
-const connection = knex(environmentConfig);
+// const environment = process.env.NODE_ENV || "development";
+// const config = require("../knexfile");
+// const environmentConfig = config[environment];
+// const knex = require("knex");
+// const connection = knex(environmentConfig);
 
-module.exports = connection;
+// module.exports = connection;
+
+const environment = process.env.NODE_ENV || 'development';
+const config = require('../knexfile')[environment];
+module.exports = require('knex')(config);
--- knexfile.js
+++ knexfile.js
@@ -10,12 +10,6 @@ module.exports = {
   },
   production: {
     client: "pg",
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      ssl: { rejectUnauthorized: false },
-    },
+    connection: process.env.DATABASE_URL + "?ssl=true",
   },
 };
