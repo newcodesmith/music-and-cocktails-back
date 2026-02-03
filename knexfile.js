df805366cca36b8dd11ad6aa39548e5817411ace
Michael <milo0721@gmail.com>
2026-02-02 17:41:17 -0700

    update

--- knexfile.js
+++ knexfile.js
@@ -10,6 +10,9 @@ module.exports = {
   },
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    },
   },
 };
